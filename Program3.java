c62f171ab7233c437bff94297ae6b3d3b6fda037
SaiDeekshithaGanji <saideekshitha.ganji@gmail.com>
2024-08-13 09:47:14 -0700

    Update Program3.java
    Completed - Asked the use to input the length and the width. And show the length inputted and the width inputted, and the calculated area and the perimeter.

--- Program3.java
+++ Program3.java
@@ -3,12 +3,53 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
+public class program3{
+    public static void main(String[] args){
+        
+        //Make a Scanner
+        Scanner myScanner = new Scanner(System.in);
+        
+        //Create my variables
+        int length = 0;
+        int width = 0;
+       
+        //Ask for user input
+        System.out.print("Enter Length: ");
+        //Get input
+        length = myScanner.nextInt();
+        
+        System.out.print("Enter Width: ");
+        //Get input
+        width = myScanner.nextInt();
+        
+        //Calculate
+        int area = length*width;
+        int perimeter = 2*(length+width);
+        
+        
+        //Output
+        System.out.println();
+        System.out.println("The Length is " + length);
+        System.out.println ("The Width is " + width);
+        System.out.println ("The Area is " + area);
+        System.out.println("The perimeter is " + perimeter);        
+    }
+    
+    
+}
 
 
 
 
 //Paste console output below:
 /*
+Enter Length: 143
+Enter Width: 82
+
+The Length is 143
+The Width is 82
+The Area is 11726
+The perimeter is 450
 
 
 */
